93ddb6ae83e79a86659cdafa902bce02b8803001
bischoffisaias07-arch <bischoffisaias07@gmail.com>
2026-02-16 16:09:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ async def home(request: Request):
 
 # 3️⃣ Cargar Excel base
 try:
-    df = pd.read_excel("Inventario.xlsx")
+    df = pd.read_excel("estatica/inventario.xlsx")    
     df.columns = df.columns.str.strip().str.lower()
 except FileNotFoundError:
     df = pd.DataFrame(columns=["codigo", "descripcion", "stock"])
@@ -139,4 +139,5 @@ def modificar_producto(codigo: str, nueva_fecha: str):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
 
